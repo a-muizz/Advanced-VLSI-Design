e4012dc3aca13b81e4f0bf0bff783c1a6ea3d08c
Abdul Muizz <110739655+a-muizz@users.noreply.github.com>
2025-03-19 02:14:21 -0400

    Add files via upload

---
+++ Project1 - FIR Filter/3Filter Reduced Parallel L3/SystemVerilog/testbench/SineWave.sv
@@ -0,0 +1,20 @@
+module SineWave
+#(parameter ADDR_WIDTH = 9, DATA_WIDTH = 16)
+(
+input                   clk,
+input  [ADDR_WIDTH-1:0] r_addr,
+output [DATA_WIDTH-1:0] r_data_1,
+output [DATA_WIDTH-1:0] r_data_2,
+output [DATA_WIDTH-1:0] r_data_3
+);
+reg [DATA_WIDTH-1 : 0] sine [0 : (2**ADDR_WIDTH)-1];
+  
+initial
+        $readmemb("binary_signal.data", sine); 
+
+assign r_data_1 = sine[r_addr];
+assign r_data_2 = sine[r_addr+1];
+assign r_data_3 = sine[r_addr+2];
+
+
+endmodule
